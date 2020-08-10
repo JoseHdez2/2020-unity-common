b61b5f2e0564856b6b34cb90f7d54cb279927a00
Jose Hernandez <josemanuelhdez2@gmail.com>
2020-08-10 22:05:04 +0100

    add InputManagerSingleton

---
+++ Scripts/InputMasterSingleton.cs
@@ -0,0 +1,20 @@
+﻿using ExtensionMethods;
+using RotaryHeart.Lib.SerializableDictionary;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class InputMasterSingleton
+{
+    public static InputMaster controls;
+    
+    public static InputMaster Get()
+    {
+        if (controls == null)
+        {
+            controls = new InputMaster();
+        }
+        return controls;
+    }
+}
