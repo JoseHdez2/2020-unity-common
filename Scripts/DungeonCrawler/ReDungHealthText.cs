b76d7c57a522ea796f63879bd74eba9bafc0dc44
Jose Hernandez <josemanuelhdez2@gmail.com>
2020-08-15 14:06:24 +0100

    ReDung Health Text

---
+++ Scripts/DungeonCrawler/ReDungHealthText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ReDungHealthText : MonoBehaviour
+{
+    private DungeonCrawlerPlayer player;
+    public int textSizePercent = 150;
+   
+    void Update()
+    {
+        if(player == null)
+        {
+            player = FindObjectOfType<DungeonCrawlerPlayer>();
+        }
+    }
+
+    public void UpdateText(int playerHealth)
+    {
+        int maxHp = FindObjectOfType<ReDungLevelInterpreter>().curLevelPlayerMaxHp;
+        string maxHpStr = maxHp > 3 ? maxHp.ToString() : $"<color=\"red\">{maxHp}</color>";
+        string str = $"<size={textSizePercent}%><smallcaps>Hp:</smallcaps> {playerHealth}<sub> / {maxHp}</sub>";
+        GetComponent<TMP_Text>().text = str;
+    }
+}
