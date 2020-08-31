5c67907a6ed3118a6a7328b1f18102f6224c1161
Jose Hernandez <josemanuelhdez2@gmail.com>
2020-08-31 17:30:11 +0100

    animation hanging, button sprite

---
+++ Scripts/Graphics/AnimHanging.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimHanging : MonoBehaviour
+{
+    [Range(0,359)]
+    public float initialDegrees = 0;
+    [Range(0,360)]
+    public float rangeOfDegrees = 90;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, initialDegrees + Mathf.Sin(Time.time) * rangeOfDegrees));
+    }
+}
