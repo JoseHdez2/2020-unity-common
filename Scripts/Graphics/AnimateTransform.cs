fd39ccb2377f0d9ceca70edbd4d06b6884d7ab9b
Jose Hernandez <josemanuelhdez2@gmail.com>
2020-09-06 20:03:29 +0100

    TitleMenuScene, AnimateTransform, ItemDatabase

---
+++ Scripts/Graphics/AnimateTransform.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using RotaryHeart.Lib.SerializableDictionary;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+[SerializeField]
+public enum Attribute { POS_X, POS_Y, SCALE, ROT_Z }
+
+[SerializeField]
+public class MyTransformAnimation {
+    [SerializeField] public Attribute attr;
+    [SerializeField] public AnimationCurve curve = AnimationCurve.EaseInOut(0, 0, 0, 1);
+    [SerializeField] public float valueRange = 0f;
+}
+
+public class AnimateTransform : MonoBehaviour {
+    [SerializeField] public List<MyTransformAnimation> anims;
+    public MyTransformAnimation anim;
+    private Vector3 initialPos, initialScale;
+    private Quaternion initialRot;
+
+    // Use this for initialization
+    void Start()
+    {
+        initialPos = transform.position;
+        initialRot = transform.rotation;
+        initialScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = initialPos;
+        transform.rotation = initialRot;
+        transform.localScale = initialScale;
+        anims.ForEach(anim => UpdateAnim(anim));
+    }
+
+    private void UpdateAnim(MyTransformAnimation anim)
+    {
+        float value = anim.curve.Evaluate(Time.time);
+        switch (anim.attr) {
+            case Attribute.POS_X: transform.position += new Vector3(value, 0, 0); break;
+            case Attribute.POS_Y: transform.position += new Vector3(0, value, 0); break;
+            case Attribute.SCALE: transform.localScale += new Vector3(0, value, 0); break;
+            case Attribute.ROT_Z: transform.rotation = Quaternion.Euler(new Vector3(0, 0, value)); break;
+        }
+    }
+}
---
+++ Scripts/ItemDatabase.cs
@@ -0,0 +1,58 @@
+﻿using ExtensionMethods;
+using RotaryHeart.Lib.SerializableDictionary;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public enum ItemType
+{
+    NONE, COPPER_ORE, IRON_ORE, TITANIUM_ORE, OAK_WOOD, BIRCH_WOOD, ASH_WOOD, COPPER_SHOVEL,
+    COPPER_PICKAXE, COPPER_AXE, IRON_SHOVEL, IRON_PICKAXE, IRON_AXE, TITANIUM_SHOVEL,
+    TITANIUM_PICKAXE, TITANIUM_AXE, HERB, CELERY, CARROT, VEGETABLE_SOUP, APPLE, EGG, APPLE_PIE,
+    GUN_POWDER, VESSEL, BOMB, PEAR, FRUIT_SALAD, MEAT, STEW, JAM, RUBY, GOLD_POWDER, DOLLAR, FRAME,
+    DOLLAR_FRAME, RUBY_RING, COIN, FLIMSY_CROWN, STURDY_CROWN, EGGNOG
+}
+
+[System.Serializable]
+public class ItemEntry{
+    [SerializeField] public DungeonCrawlerTile type;
+    [SerializeField] public int price;
+    [SerializeField] public Sprite sprite;
+    [SerializeField] public Color color;
+    [SerializeField] public string floorsItAppearsInStr;
+    [NonSerialized] public List<int> floorsItAppearsIn;
+    [SerializeField] public string toolRequiredStr;
+    [NonSerialized] public List<ItemType> toolRequired;
+};
+
+[Serializable]
+[CreateAssetMenu(fileName = "item_db", menuName = "ScriptableObjects/ItemDatabase", order = 1)]
+public class ItemDatabase : ScriptableObject
+{
+    public SerializableDictionaryBase<ItemType, ItemEntry> list;
+
+    public void Initialize(){
+        list.ToList()
+            .Where(p => !string.IsNullOrEmpty(p.Value.floorsItAppearsInStr)).ToList()
+            .ForEach(p => p.Value.floorsItAppearsIn = p.Value.floorsItAppearsInStr
+                                                                .Split(',').Select(s => int.Parse(s))
+                                                                .ToList());
+        list.ToList().Where(p => !string.IsNullOrEmpty(p.Value.floorsItAppearsInStr)).ToList()
+            .ForEach(p => p.Value.toolRequired = p.Value.toolRequiredStr
+                                                        .Split(',').Select(s => ParseTool(s))
+                                                        .ToList());
+    
+    }
+
+    private ItemType ParseTool(string s)
+    {
+        var list = new List<ItemType>();
+        foreach (ItemType itemType in Enum.GetValues(typeof(ItemType))) {
+            list.Add(itemType);
+        }
+        return list.Find(it => it.ToString() == s);
+    }
+}
