280a255f93b4f39c3713bed4fafdd3345ecd42b8
Jose Hernandez <josemanuelhdez2@gmail.com>
2020-09-14 00:57:32 +0100

    button menu should give focus to the first button.

---
+++ Scripts/UI/ButtonUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonUI : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    public bool selected = false;
+    Vector2 offMax;
+    Vector2 offMin;
+    RectTransform myRT;
+    void Start()
+    {
+        myRT = transform as RectTransform;
+        offMax = myRT.offsetMax;
+        offMin = myRT.offsetMin;
+    }
+
+    public void OnSelect(BaseEventData data)
+    {
+        myRT.offsetMax = offMax * 1.1f;
+        myRT.offsetMin = offMin * 1.1f;
+        selected = true;
+    }
+    public void OnDeselect(BaseEventData data)
+    {
+        myRT.offsetMax = offMax;
+        myRT.offsetMin = offMin;
+        selected = false;
+    }
+}
