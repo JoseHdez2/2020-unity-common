6e9df002b9697483e23f46026f2192b5cfd98838
JoseHdez2 <josemanuelhdez2@gmail.com>
2020-10-31 17:12:40 +0000

    ItemDraggable and ItemDraggableSlot.

---
+++ Scripts/UI/DragDropSlot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropSlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData){
+        Debug.Log("OnDrop");
+        if(eventData.pointerDrag != null){
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
