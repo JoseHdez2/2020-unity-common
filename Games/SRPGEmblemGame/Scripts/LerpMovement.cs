e4174ca9981935452f7b751047349ad38e8d8c1a
JoseHdez2 <josemanuelhdez2@gmail.com>
2020-11-15 20:11:18 +0000

    Android build. fix: use delta time.

--- Games/SRPGEmblemGame/Scripts/LerpMovement.cs
+++ Games/SRPGEmblemGame/Scripts/LerpMovement.cs
@@ -4,12 +4,11 @@ public class LerpMovement : MonoBehaviour {
     
     public Vector3? destinationPos;
 
-    [Range(0,1)]
-    public float speed = 0.05f;
+    public float speed = 1f;
 
     public void Update() {
         if(destinationPos.HasValue){
-            transform.position = Vector3.Lerp(transform.position, destinationPos.Value, speed);
+            transform.position = Vector3.Lerp(transform.position, destinationPos.Value, speed * Time.deltaTime);
             if(Vector3.Distance(transform.position, destinationPos.Value) < 0.05){
                 transform.position = destinationPos.Value;
                 destinationPos = null;
