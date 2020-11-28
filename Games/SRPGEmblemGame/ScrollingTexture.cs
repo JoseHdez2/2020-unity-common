fe6c2e827a0ad2704d8a965be60e69cadabcbac9
JoseHdez2 <josemanuelhdez2@gmail.com>
2020-11-28 20:59:50 +0000

    sprite scrolling shader (2 days ago)

---
+++ Games/SRPGEmblemGame/ScrollingTexture.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ScrollingTexture : MonoBehaviour {
+    public Vector2 ScrollSpeed;
+ 
+    private void OnEnable() {
+        GetComponent<SpriteRenderer>().material.SetVector("_ScrollSpeed", ScrollSpeed);
+    }
+}
