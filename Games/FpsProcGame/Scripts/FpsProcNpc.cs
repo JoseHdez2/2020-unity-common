aad2bdca9b642d5136324f32f0c3a09fc4c3ef17
JoseHdez2 <josemanuelhdez2@gmail.com>
2020-12-06 00:30:57 +0000

    procjam: npcs show their name above their head

--- Games/FpsProcGame/Scripts/FpsProcNpc.cs
+++ Games/FpsProcGame/Scripts/FpsProcNpc.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using ExtensionMethods;
 using UnityEngine;
+using TMPro;
 
 [System.Serializable]
 public class FpsProcNpcData {
@@ -14,13 +15,14 @@ public class FpsProcNpc : MonoBehaviour
     public FpsProcNpcData data = new FpsProcNpcData();
     public Texture2D face;
     public MeshRenderer faceRenderer;
-    public Shader faceShader;
+    [SerializeField] private TMP_Text textName;
 
     // Start is called before the first frame update
     void Start(){
         FpsProcDatabase db = FindObjectOfType<FpsProcDatabase>();
         data.fullName = db.GetRandomFullName();
         name = data.fullName;
+        textName.text = data.fullName;
         // faceRenderer.material = new Material(faceShader);
         data.faceIndex = db.GetRandomFaceIndex();
         faceRenderer.material.mainTexture = FpsProcDatabase.faces[data.faceIndex];
