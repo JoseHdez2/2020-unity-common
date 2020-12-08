cf3b051ac29f42e67f2a049dc1a58a82720a7936
JoseHdez2 <josemanuelhdez2@gmail.com>
2020-12-08 23:24:29 +0000

    procjam: district of buildings

--- Games/FpsProcGame/Scripts/FpsProcDistrict.cs
+++ Games/FpsProcGame/Scripts/FpsProcDistrict.cs
@@ -1,21 +1,34 @@
 using System.Collections.Generic;
 using System.Linq;
+using ExtensionMethods;
+using RotaryHeart.Lib.SerializableDictionary;
 using UnityEngine;
 
 public class FpsProcDistrict : MonoBehaviour {
     
-    [SerializeField] List<FpsProcBldg> buildings;
-    [SerializeField] private Vector3Int gridSize;
-    [SerializeField] private int npcAmount;
+    [TextArea(5,10)]
+    public string tilemap;
+    public SerializableDictionaryBase<char, FpsProcBldg> prefabs;
+    [SerializeField] private Vector3Int cellScale;
 
     public void Start(){
         GenerateAndInstantiateBuildings();
     }
 
     public void GenerateAndInstantiateBuildings(){
-        buildings = GetComponentsInChildren<FpsProcBldg>().ToList();
-        foreach(FpsProcBldg building in buildings){
-            GenerateAndInstantiate(building);
+        string[] rows = tilemap.Split('\n');
+        for(int y = 0; y < rows.Length; y++){
+            for(int x = 0; x < rows[y].Length; x++){
+                FpsProcBldg pf;
+                prefabs.TryGetValue(rows[y][x], out pf);
+                if(pf == null){
+                    Debug.LogError($"No prefab for character '{rows[y][x]}'.");
+                } else {
+                    FpsProcBldg bldg = Instantiate(pf, Vector3.zero, Quaternion.identity, transform);
+                    bldg.data.origin = new Vector3(y, 0, x).ScaleWith(cellScale);
+                    GenerateAndInstantiate(bldg);
+                }
+            }
         }
     }
 
