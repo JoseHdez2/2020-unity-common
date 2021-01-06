7004518f38aad3d492e9068b80ac93afefc854fb
JoseHdez2 <josemanuelhdez2@gmail.com>
2021-01-06 23:34:11 +0000

    detective game: choosing dialog options works!

--- Games/DetectiveGame/DetectiveGameMgr.cs
+++ Games/DetectiveGame/DetectiveGameMgr.cs
@@ -6,12 +6,14 @@ using ExtensionMethods;
 using System;
 using System.Linq;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 [Serializable]
 public class JsonDialog {
     [SerializeField] public JsonDialogLine[] lines;
     public int index;
     public int Length() => lines.Length;
+    public JsonDialogLine curLine() => lines[index];
 }
 
 [Serializable]
@@ -30,10 +32,12 @@ public class JsonDialogOption : JsonDialog {
 public class DetectiveGameMgr : MonoBehaviour
 {
     [SerializeField] private AudioSourceDetective audioSourceDetective; 
-    [SerializeField] private AnimFade blackScreen, whiteScreen, charFade;
+    [SerializeField] private AnimFade blackScreen, whiteScreen, charFade, dialogChoicesFade;
     [SerializeField] private DialogueManager dialogueManager;
     [SerializeField] private DialogBubbleUI nameBubble;
     [SerializeField] private Image charImage;
+    [SerializeField] private ButtonMenu dialogChoices;
+    private bool isChoosing = false;
     
     public TextAsset[] dialogs;
     public Texture2D[] charImages;
@@ -41,6 +45,7 @@ public class DetectiveGameMgr : MonoBehaviour
     public ObjectShake objectShake;
     void Start(){
         blackScreen.Toggle(true);
+        dialogChoicesFade.Toggle(false);
         dialogStack.Push(JsonUtility.FromJson<JsonDialog>(dialogs[0].text));
     }
 
@@ -50,8 +55,14 @@ public class DetectiveGameMgr : MonoBehaviour
         // if(blackScreen.IsDone() && dialogueManager.isDone && dialogInd < curDialog.Length){
         //     ProcessNewLine();
         // }
-        if(blackScreen.IsDone() && dialogueManager.isDone && curJsonDialog().index < curJsonDialog().Length()){
-            ProcessNewLine();
+        if(blackScreen.IsDone() 
+            && dialogueManager.isDone 
+            && !isChoosing){
+                if(curJsonDialog().index < curJsonDialog().Length()){
+                    ProcessNewLine();
+                } else {
+                    dialogStack.Pop();
+                }
         }
     }
 
@@ -59,14 +70,14 @@ public class DetectiveGameMgr : MonoBehaviour
     private void ProcessNewLine() {
         JsonDialogLine line = curJsonDialog().lines[curJsonDialog().index];
         if(line.options != null && !line.options.IsEmpty()){
-            Debug.Log("options!");
+            ShowOptions(line.options);
         } else { 
             if(!line.raw.IsNullOrWhiteSpace()) {
                 line = ParseLine(line.raw);
             }
             ProcessLine(line.actor, line.dialog);
+            curJsonDialog().index++;
         }
-        curJsonDialog().index++;
     }
 
     private JsonDialogLine ParseLine(string rawLine) {
@@ -120,6 +131,32 @@ public class DetectiveGameMgr : MonoBehaviour
         }
         line = pattern.Replace(line, "");        
         return line;
+    }
+
+    private void ShowOptions(JsonDialogOption[] options)
+    {
+        isChoosing = true;
+        dialogChoices.ClearButtonData();
+        for(int i = 0; i < options.Length; i++){
+            UnityEvent unityEvent = new UnityEvent();
+            ButtonData buttonData = new ButtonData(){name= options[i].option, action= unityEvent, index= i};
+            unityEvent.AddListener(() => ChooseDialogOption(buttonData.index));
+            dialogChoices.AddButtonData(buttonData);
+        }
+        dialogChoices.RefreshButtons();
+        dialogChoicesFade.Toggle(true);
+    }
+
+    public void ChooseDialogOption(int index){
+        dialogChoicesFade.Toggle(false);
+        dialogChoices.ClearButtonData();
+        dialogChoices.RefreshButtons();
+        Debug.Log($"Chose option with index {index}.");
+        JsonDialogLine jsonDialogLine = curJsonDialog().curLine();
+        JsonDialogOption jsonDialogOption = jsonDialogLine.options[index];
+        jsonDialogOption.index = 0; // if you choose the same option again, reset it.
+        dialogStack.Push(jsonDialogOption);
+        isChoosing = false;
     }
 
     private IEnumerator CrShowChar(string filename){
