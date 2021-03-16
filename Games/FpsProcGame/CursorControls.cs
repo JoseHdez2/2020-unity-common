017ecfe259587978f71b70ef588567b6ad8abfce
arbust8 <49127983+arbust8@users.noreply.github.com>
2021-03-16 19:49:38 +0000

    Termination: Cursor added

---
+++ Games/FpsProcGame/CursorControls.cs
@@ -0,0 +1,132 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/2020-unity-common/Games/FpsProcGame/CursorControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @CursorControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @CursorControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""CursorControls"",
+    ""maps"": [
+        {
+            ""name"": ""Mouse"",
+            ""id"": ""e78e08d8-badc-4c0a-a519-733318761929"",
+            ""actions"": [
+                {
+                    ""name"": ""Click"",
+                    ""type"": ""Button"",
+                    ""id"": ""12c69e53-6c13-4893-9548-0095adcbcea1"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""b880320e-9c15-40af-9a58-6dc25177b547"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": ""Press(behavior=1)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Click"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Mouse
+        m_Mouse = asset.FindActionMap("Mouse", throwIfNotFound: true);
+        m_Mouse_Click = m_Mouse.FindAction("Click", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Mouse
+    private readonly InputActionMap m_Mouse;
+    private IMouseActions m_MouseActionsCallbackInterface;
+    private readonly InputAction m_Mouse_Click;
+    public struct MouseActions
+    {
+        private @CursorControls m_Wrapper;
+        public MouseActions(@CursorControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Click => m_Wrapper.m_Mouse_Click;
+        public InputActionMap Get() { return m_Wrapper.m_Mouse; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MouseActions set) { return set.Get(); }
+        public void SetCallbacks(IMouseActions instance)
+        {
+            if (m_Wrapper.m_MouseActionsCallbackInterface != null)
+            {
+                @Click.started -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
+                @Click.performed -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
+                @Click.canceled -= m_Wrapper.m_MouseActionsCallbackInterface.OnClick;
+            }
+            m_Wrapper.m_MouseActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Click.started += instance.OnClick;
+                @Click.performed += instance.OnClick;
+                @Click.canceled += instance.OnClick;
+            }
+        }
+    }
+    public MouseActions @Mouse => new MouseActions(this);
+    public interface IMouseActions
+    {
+        void OnClick(InputAction.CallbackContext context);
+    }
+}
---
+++ Games/FpsProcGame/Scripts/CursorController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+
+    public Texture2D cursor;
+    public Texture2D cursorClick;
+    private CursorControls controls;
+
+    private void Awake() {
+        controls = new CursorControls();
+        ChangeCursor(cursor);
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    private void OnEnable() {
+        controls.Enable();
+    }
+
+    private void OnDisable() {
+        controls.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls.Mouse.Click.started += _ => StartedClick();
+        controls.Mouse.Click.performed += _ => EndedClick();
+    }
+
+    private void StartedClick(){
+        ChangeCursor(cursorClick);
+    }
+
+    private void EndedClick(){
+        ChangeCursor(cursor);
+    }
+
+    private void ChangeCursor(Texture2D cursorType){
+        //Vector2 hotspot = new Vector2(0, cursorType.height);
+        Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto);        
+    }
+}
